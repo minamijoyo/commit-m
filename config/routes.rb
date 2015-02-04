f44bf476e2210d3ca18ec5f682ada5f7afd2ca82
minamijoyo <minamijoyo@gmail.com>
2015-02-04 23:13:18 +0900

    Add route commits#search via get

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'commits#index'
-  match 'commits/search', to: 'commits#search', via: 'post'
+  match 'commits/search', to: 'commits#search', via: ['post', 'get']
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
