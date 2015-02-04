b15dc1093501c7c553de8567ff6f55731809ca30
minamijoyo <minamijoyo@gmail.com>
2015-02-04 17:33:28 +0900

    Devide index.html to _footer, _search_form, search

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,5 +1,9 @@
 class CommitsController < ApplicationController
   def index
+    @commits = []
+  end
+
+  def search
     @commits = Commit.paginate(page: params[:page])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'commits#index'
+  match 'commits/search', to: 'commits#search', via: 'post'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
