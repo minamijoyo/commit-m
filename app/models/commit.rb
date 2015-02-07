0895659965fd38ce7b7d4de2c9ea0eaf1c62926d
minamijoyo <minamijoyo@gmail.com>
2015-02-07 19:01:39 +0900

    Add search results count

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,6 +1,10 @@
 class Commit < ActiveRecord::Base
   def self.search_message(keyword)
-    against_key = keyword.split.map { |key| key.insert(0,'+') }.join(' ')
-    where("match(message) against (? in boolean mode)", "#{against_key}")
+    if keyword.present?
+      against_key = keyword.split.map { |key| key.insert(0,'+') }.join(' ')
+      where("match(message) against (? in boolean mode)", "#{against_key}")
+    else
+      Commit.none
+    end
   end
 end
