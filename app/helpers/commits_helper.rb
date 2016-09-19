fec1b0ae870c2e0282ae785cc70f8c5979c59608
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-09-19 12:38:01 +0900

    Highlight by ElasticSearch

--- app/helpers/commits_helper.rb
+++ app/helpers/commits_helper.rb
@@ -1,2 +1,6 @@
 module CommitsHelper
+  def highlight_message(commit)
+    message = commit.try!(:highlight).try!(:message).try!(:join) || commit.message
+    sanitize(message, tags: %w[mark])
+  end
 end
